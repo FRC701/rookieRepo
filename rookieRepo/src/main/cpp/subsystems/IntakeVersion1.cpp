648846014245a8ef1595d5d10728fa474a75399b
rheanne <rheanneb@vandenrobotics.com>
2023-09-27 16:55:18 -0700

    anotherthing:D

--- rookieRepo/src/main/cpp/subsystems/IntakeVersion1.cpp
+++ rookieRepo/src/main/cpp/subsystems/IntakeVersion1.cpp
@@ -4,13 +4,31 @@
 
 #include "subsystems/IntakeVersion1.h"
 
-IntakeVersion1::IntakeVersion1() = default;
+IntakeVersion1::IntakeVersion1(WPI_TalonFX &ID1, WPI_TalonFX &ID2)
+    : thename(ID1), 
+    thename1(ID2)
+{
+}
 
 // This method will be called once per scheduler run
 void IntakeVersion1::Periodic() {}
 /* I wanna learn more about programmiingg
-strawberries are the best! 
+strawberries are the best!
 Cats are cute :D*/
 
-/* strawberries are the best! 
+/* strawberries are the best!
 dogs are cute too :D */
+double IntakeVersion1::rushe(double Motorspeed) {
+    thename.Set(Motorspeed);
+    return Motorspeed;
+}
+double IntakeVersion1::setting(double spin){
+    thename1.Set(spin);
+    return spin;
+}
+
+double IntakeVersion1::setrushe(double floaat){
+thename.Set(floaat);
+thename1.Set(floaat);
+return floaat;
+}
--- rookieRepo/src/main/include/subsystems/IntakeVersion1.h
+++ rookieRepo/src/main/include/subsystems/IntakeVersion1.h
@@ -5,17 +5,22 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
 
 class IntakeVersion1 : public frc2::SubsystemBase {
  public:
-  IntakeVersion1();
+  IntakeVersion1(WPI_TalonFX& ID1 , WPI_TalonFX& ID2);
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
    */
   void Periodic() override;
-
+double rushe(double Motorspeed);
+double setting(double spin);
+double setrushe(double floaat);
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+  WPI_TalonFX& thename; 
+  WPI_TalonFX& thename1;
 };
