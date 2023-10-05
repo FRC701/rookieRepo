84bd358cb5682f0071fbd141d995085b300f3ebd
Daija Canegan <daijac@vandenrobotics.com>
2023-10-05 16:37:29 -0700

    Shooter part 1

---
+++ rookieRepo/src/main/cpp/subsystems/Shooter1.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Shooter1.h"
+
+Shooter1::Shooter1(WPI_TalonFX& ID1)
+    :Shooter1(ID1)
+{
+
+}
+
+// This method will be called once per scheduler run
+void Shooter1::Periodic() {}
+
+double Shooter1::SpinShooter(double ShooterSpeed){
+    ShooterM1.Set(ShooterSpeed);
+    return ShooterSpeed;
+}
