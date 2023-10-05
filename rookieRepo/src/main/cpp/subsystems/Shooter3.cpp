36826867b40c3d6a2eab94d5a6201f28479be1c8
Kyle Kang <kylek@vandenrobotics.com>
2023-10-05 16:37:47 -0700

    Subsystem Created
    Shooter3

---
+++ rookieRepo/src/main/cpp/subsystems/Shooter3.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Shooter3.h"
+
+Shooter3::Shooter3(WPI_TalonFX& ID1)
+:Momor(ID1)
+{
+
+}
+
+// This method will be called once per scheduler run
+void Shooter3::Periodic() {}
+
+double Shooter3::SchoolShooter(double Speed)
+{
+Momor.Set(Speed);
+return Speed;
+}
